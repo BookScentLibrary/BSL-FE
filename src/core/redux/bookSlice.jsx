ef8cd14563c3870b67fcf93b62069013a07e2efd
yunju <kkyyjj1126@naver.com>
2023-10-04 13:57:18 +0900

    1004update

--- src/core/redux/bookSlice.jsx
+++ src/core/redux/bookSlice.jsx
@@ -3,14 +3,17 @@ import { userAPI } from '../api'
 
 
 
-
+//책 목록의 상태 정의 
 export const initialState = {
 
-}
+books : [], //책 목록을 저장할 배열
+testMessage : "테스트 메시지",
+
+};
 
 
-
-export const testApi = createAsyncThunk(
+//책과 관련된 액션 정의 
+export const fetchBooks = createAsyncThunk(
   'book/search',
   async (data, thunkAPI) => {
     try {
@@ -23,15 +26,28 @@ export const testApi = createAsyncThunk(
   },
 );
 
-export const userSlice = createSlice({
-  name: 'userReducer',
+export const bookSlice = createSlice({
+  name: 'bookReducer',
   initialState,
   reducers: {
-    setUser: (state, action) => {
-      state.user = action.payload;
-      state.is_login = true;
-      return;
+    setTestMessage: (state, action) => {
+      state.testMessage = action.payload;
+    }
     },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchBooks.pending, (state) => {
+          //요청이 보내지기 전에 상태 업데이트 
+        })
+        .addCase(fetchBooks.fulfilled, (state, action) => {
+          //요청이 성공했을 때 책 목록 업데이트 
+          state.books = action.payload;
+        })
+        .addCase(fetchBooks.rejected, (state,action) => {
+          //요청이 실패했을 때 처리할 내용을 여기에 추가 가능함.
+        })
+
+        
  
   },
 });
