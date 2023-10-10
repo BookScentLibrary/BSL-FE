da84aab5a3cdd5735158fe8f2cf32b5751609a69
nongbu24 <pop524@naver.com>
2023-10-10 20:12:47 +0900

    feat #3 odd style, bookCnt

--- src/components/new/NewBooksList.jsx
+++ src/components/new/NewBooksList.jsx
@@ -1,35 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const NewBooksMain = () => {
     const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const newBooksData = Array.from({ length: 20 }, (_, index) => ({
+            newBooksNo: index + 1
+        }));
+        setItems(newBooksData);
+    }, []);
+
     return (
         <div>
             <h1>신간 도서</h1>
             <hr />
             <Pd>
-                최근 30일 이내 입수된 도서 목록입니다. //여기 버튼 모양 이상해요
+                최근 30일 이내 입수된 도서 목록입니다.
                 <Btn>신간 도서 등록</Btn>
             </Pd>
             <hr />
             <table>
                 <thead>
-                        <Other>글번호</Other>
-                        <Title>제목</Title>
-                        <Other>저자</Other>
-                        <Other>발행처</Other>
-                        <Other>발행년도</Other>
-                        <Date>입수일</Date>
+                    <Other>글번호</Other>
+                    <Title>제목</Title>
+                    <Other>저자</Other>
+                    <Other>발행처</Other>
+                    <Other>발행년도</Other>
+                    <Date>입수일</Date>
                 </thead>
                 <tbody>
-                    {items.map((items) => (
-                        <tr key={items.bookNo}>
-                            <td>{items.bookNo}</td>
-                            <td>{items.bookname}</td>
-                            <td>{items.author}</td>
-                            <td>{items.publisher}</td>
-                            <td>{items.publicationYear}</td>
-                            <td>{items.regDate}</td>
+                    {items.map((item) => (
+                        <tr key={item.newBooksNo}>
+                            <td>{item.bookNo}</td>
+                            <td>{item.bookname}</td>
+                            <td>{item.author}</td>
+                            <td>{item.publisher}</td>
+                            <td>{item.publicationYear}</td>
+                            <td>{item.regDate}</td>
                         </tr>
                     ))}
                 </tbody>
@@ -42,12 +50,12 @@ const Btn = styled.button`
     float: right;
     background: ${({ color, theme }) =>
         color
-        ? color === "red"
-        ? theme.colors.secondary
-        : color === "gray"
-        ? theme.colors.gray
-        : theme.colors.primary
-        : theme.colors.primary};
+            ? color === "red"
+                ? theme.colors.secondary
+                : color === "gray"
+                ? theme.colors.gray
+                : theme.colors.primary
+            : theme.colors.primary};
     border-radius: 3px;
     width: ${({ width }) => (width ? width : "110px")};
     height: ${({ height }) => (height ? height : "35px")};
@@ -57,12 +65,12 @@ const Btn = styled.button`
     &:hover {
         background: ${({ color, theme }) =>
             color
-            ? color === "red"
-            ? theme.colors.darkred5
-            : color === "gray"
-            ? theme.colors.grayhover
-            : theme.colors.darkgreen5
-            : theme.colors.darkgreen5};
+                ? color === "red"
+                    ? theme.colors.darkred5
+                    : color === "gray"
+                    ? theme.colors.grayhover
+                    : theme.colors.darkgreen5
+                : theme.colors.darkgreen5};
     }
 `;
 
@@ -83,5 +91,3 @@ const Date = styled.th`
 `;
 
 export default NewBooksMain;
-
-//데이터베이스에서 regDate 큰 순으로 정렬해서 20개를 추출해야함
