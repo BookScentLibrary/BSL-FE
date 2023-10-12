2307ae804e2a604ab26186cda70804662d40a703
river <playamongthestars000@gmail.com>
2023-10-12 23:51:33 +0900

    feat #1 : 공용 컴포넌트(menu, input) 수정

--- src/pages/newspart/NewsPartMain.jsx
+++ src/pages/newspart/NewsPartMain.jsx
@@ -3,29 +3,50 @@ import React from "react";
 import Menu from "../../components/shared/comp/menu/Menu";
 import styled from "styled-components";
 import ReviewListPage from "../review/ReviewListPage";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const NewsPartMain = () => {
+  const navigate = useNavigate();
+  const pathname = useLocation().pathname;
   const title = "소식·참여";
-  const menuArr = ["공지사항", "프로그램 안내", "리뷰게시판"];
   const [pageIdx, setPageIdx] = React.useState(0);
-  console.log(pageIdx);
   const page = {
-    // 0: <Notice/>,
-    // 1: <Program/>,
     0: "",
     1: "",
     2: <ReviewListPage />,
   };
 
+  const goToNotice = () => {};
+
+  const goToProgram = () => {};
+
+  const goToReview = () => {
+    setPageIdx(2);
+    navigate("/news/reviewList");
+  };
+
+  React.useEffect(() => {
+    if (pathname === "/news") {
+      setPageIdx(0);
+    } else if (pathname.split("/")[2] === "reviewList") {
+      setPageIdx(2);
+    }
+  }, [pathname]);
+
   return (
     <React.Fragment>
       <Container>
-        <Menu
-          title={title}
-          menuArr={menuArr}
-          pageIdx={pageIdx}
-          setPageIdx={setPageIdx}
-        />
+        <Menu title={title} selected={pageIdx}>
+          <p className="menu_first" onClick={goToNotice}>
+            공지사항
+          </p>
+          <p className="menu_second" onClick={goToProgram}>
+            프로그램 안내
+          </p>
+          <p className="menu_third" onClick={goToReview}>
+            리뷰게시판
+          </p>
+        </Menu>
         {page[pageIdx]}
       </Container>
     </React.Fragment>
